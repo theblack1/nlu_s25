184eaf0636b5205ef1584e0d3520c330d950ebed
theblack1 <2049132737@qq.com>
2025-03-30 17:38:23 -0400

    change words

--- hw3/hpc/run_truthfulqa_evaluation.sh
+++ hw3/hpc/run_truthfulqa_evaluation.sh
@@ -1,7 +1,12 @@
 # Add all your Python calls here:
-python truthfulqa.py facebook/opt-125m
-python truthfulqa.py facebook/opt-350m
-python truthfulqa.py facebook/opt-1.3B
-python truthfulqa.py facebook/opt-2.7B
-python truthfulqa.py facebook/opt-6.7B
+# python truthfulqa.py facebook/opt-125m
+# python truthfulqa.py facebook/opt-350m
+# python truthfulqa.py facebook/opt-1.3B
+# python truthfulqa.py facebook/opt-2.7B
+# python truthfulqa.py facebook/opt-6.7B
+python /home/fl2656/nlu_s25/hw3/truthfulqa.py facebook/opt-125m
+python /home/fl2656/nlu_s25/hw3/truthfulqa.py facebook/opt-350m
+python /home/fl2656/nlu_s25/hw3/truthfulqa.py facebook/opt-1.3B
+python /home/fl2656/nlu_s25/hw3/truthfulqa.py facebook/opt-2.7B
+python /home/fl2656/nlu_s25/hw3/truthfulqa.py facebook/opt-6.7B
 # ... etc.
